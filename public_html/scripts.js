0547d43fa8033afce03c7d2ab0ea36b773df43fa
Sarvarbek Usmonov <sarvarbekusmonov@Sarvarbeks-MacBook-Pro.local>
2023-03-20 18:11:32 -0700

    Update

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,37 +1,62 @@
+/*
+  Author: Sarvarbek Usmonov
+  File: scripts.js
+  Purpose: Code for user side js. 
+  Make requests to server and updates page when certain actions are done or
+  in every 1s
+*/
+
+// Get references to the chat window, input fields, and send button
 const chatWindow = document.getElementById('chat-window');
 const aliasInput = document.getElementById('alias');
 const messageInput = document.getElementById('message');
 const sendBtn = document.getElementById('send-btn');
 
+// Add a click event listener to the send button
 sendBtn.addEventListener('click', async () => {
+  // Get the values from the input fields
   const alias = aliasInput.value;
   const message = messageInput.value;
 
+  // If either input field is empty, do nothing and return
   if (!alias || !message) return;
 
+  // Send a POST request with the alias and message in the request body
   await fetch('/chats/post', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ alias, message }),
   });
 
+  // Clear the message input field after sending the message
   messageInput.value = '';
 });
 
+// Define an async function to fetch the messages from the server
 async function fetchMessages() {
+  // Send a GET request to the server to fetch the messages
   const response = await fetch('/chats');
   const messages = await response.json();
+  
+  // Display the messages in the chat window
   displayMessages(messages);
 }
 
+// Define a function to display the messages in the chat window
 function displayMessages(messages) {
+  // Clear the chat window before displaying new messages
   chatWindow.innerHTML = '';
+
+  // Loop through the messages and add each one to the chat window
   messages.forEach((message) => {
     const messageElement = document.createElement('p');
     messageElement.innerHTML = `<strong>${message.alias}:</strong> ${message.message}`;
     chatWindow.appendChild(messageElement);
   });
+
+  // Scroll to the bottom of the chat window to show the latest messages
   chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
+// Call the fetchMessages function every second to update the chat window
 setInterval(fetchMessages, 1000);
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+/*
+  Author: Sarvarbek Usmonov
+  File: server.js
+  Purpose: Server side code for chat app.
+  Uses mongoDB to store data, recieves get request, sends post requests.
+*/
+
+// Require the necessary modules
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+// Create a new Express app
 const app = express();
+
+// Set the port number for the server
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the public_html directory
 app.use(express.static('public_html'));
+
+// Parse incoming JSON data with the body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set up the MongoDB connection
 const mongoURI = 'mongodb://127.0.0.1:27017/chatty';
 mongoose.connection.on("connected", () => console.log("Connected"))
 mongoose.connection.on("error", (err) => console.log(err))
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Define the schema for the chat messages
 const Schema = mongoose.Schema;
 const ChatMessageSchema = new Schema({
   time: Number,
@@ -20,13 +37,16 @@ const ChatMessageSchema = new Schema({
   message: String,
 });
 
+// Create a new model for the chat messages
 const ChatMessage = mongoose.model('ChatMessage', ChatMessageSchema);
 
+// Define a route to fetch all chat messages from the server
 app.get('/chats', async (req, res) => {
   const messages = await ChatMessage.find().sort('time');
   res.json(messages);
 });
 
+// Define a route to post a new chat message to the server
 app.post('/chats/post', async (req, res) => {
   const newMessage = new ChatMessage({
     time: Date.now(),
@@ -38,6 +58,7 @@ app.post('/chats/post', async (req, res) => {
   res.sendStatus(200);
 });
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
